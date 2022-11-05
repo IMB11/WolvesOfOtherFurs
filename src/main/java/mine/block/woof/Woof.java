8b0969ddaa1d561a0c41789d37a3a5192204cfc0
mineblock11 <93472213+mineblock11@users.noreply.github.com>
2022-11-05 21:36:31 +0000

    License Headers

--- src/main/java/mine/block/woof/Woof.java
+++ src/main/java/mine/block/woof/Woof.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2022 mineblock11 <https://github.com/mineblock11>
+ *
+ * All code in Wolves Of Other Furs is licensed under the Academic Free License version 3.0
+ */
+
 package mine.block.woof;
 
 import mine.block.woof.api.Variant;
